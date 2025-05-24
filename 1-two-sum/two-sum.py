84bf153e1f47528551a7bcc8908bc5432f54e59d
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-05-23 18:03:51 -0700

    Time: 3 ms (50.83%) | Memory: 18.9 MB (34.04%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashmap.keys():
+                return [hashmap[diff], i]
+            hashmap[nums[i]] = i
+        return [-1,-1]
