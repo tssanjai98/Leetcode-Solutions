bf8775b4f5502dd2ff6d969d71c7a3f92719acdb
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-07-07 23:03:29 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (14.86%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,30 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy = ListNode(0)
-        current = dummy
-        carry = 0
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        temp = ListNode()
+        curr = temp
 
+        carry = 0
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            total = val1 + val2 + carry
-            carry = total // 10
-            current.next=ListNode(total % 10)
-            current = current.next
 
-            if l1:
-                l1=l1.next
-            if l2:
-                l2 = l2.next
+            val = val1 + val2 + carry
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
 
-        return dummy.next
+            curr = curr.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        return temp.next
