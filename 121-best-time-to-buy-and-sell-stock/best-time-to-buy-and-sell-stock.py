dd52c302e878c0f9278e2801676e090f595a2dcc
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-08-26 13:16:26 -0400

    Time: 103 ms (41.76%) | Memory: 26.9 MB (77.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,14 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        minPrice = float('inf')
-        maxProf = 0
-        for i in range(len(prices)):
-            minPrice = min(prices[i], minPrice)
-            diff = prices[i] - minPrice
-            maxProf = max(diff, maxProf)
-        return maxProf
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                prof = prices[r] - prices[l]
+                maxP = max(maxP, prof)
+            else:
+                l = r
+            r+=1
+
+        return maxP
