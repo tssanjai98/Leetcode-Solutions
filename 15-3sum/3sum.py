2638e0002a7903fc763becb10fd6fcb2966e51db
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-08-27 15:01:53 -0400

    Time: 533 ms (75.64%) | Memory: 20.7 MB (69.68%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,24 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = set()
-        nums_sorted = sorted(nums)
-        for i in range(len(nums)):
-            j = i+1
-            k = len(nums_sorted) - 1 
-            while(j<k):
-                tsum = nums_sorted[i] + nums_sorted[j] + nums_sorted[k]
-                if(tsum == 0):
-                    res.add(tuple([nums_sorted[i] , nums_sorted[j] , nums_sorted[k]]))
-                    j+=1
-                    k-=1
-                    while(j < k and nums_sorted[k] == nums_sorted[k+1]):
-                        k-=1
-                    while(j < k and nums_sorted[j] == nums_sorted[j-1]):
-                        j+=1
-                elif(tsum > 0):
-                    k -= 1
-                    
-                elif(tsum < 0):
-                    j+=1
+        res = []
+        nums.sort()
 
-        return [list(i) for i in res]
+        for i, n in enumerate(nums):
+            if i > 0 and n == nums[i-1]:
+                continue
+
+            l,r = i+1, len(nums)-1
+            while l<r:
+                ts = n + nums[l] + nums[r]
+                if ts > 0:
+                    r-=1
+                elif ts < 0:
+                    l+=1
+                else:
+                    res.append([n, nums[l], nums[r]])
+                    l+=1
+                    while nums[l] == nums[l-1] and l<r:
+                        l+=1
+        return res
