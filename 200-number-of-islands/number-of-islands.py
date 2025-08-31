47058114999c6c15a80deb55e53fad93c0892013
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-08-31 00:00:35 -0400

    Time: 320 ms (9.25%) | Memory: 25.4 MB (22.54%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,33 +1,34 @@
-class Solution(object):
-    def numIslands(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: int
-        """
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
 
+        visited = set()
         rows, cols = len(grid), len(grid[0])
-        visit = set()
         islands = 0
 
         def bfs(r, c):
             q = collections.deque()
-            visit.add((r, c))
-            q.append((r, c))
+            visited.add((r,c))
+            q.append((r,c))
+
             while q:
-                row, col = q.popleft()
-                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                r,c = q.popleft()
+                directions = [[1,0], [-1,0],[0,1], [0,-1]]
                 for dr, dc in directions:
-                    r, c = row+dr, col+dc
-                    if (r in range(rows) and c in range(cols) and grid[r][c] == "1" and (r, c) not in visit):
-                        q.append((r, c))
-                        visit.add((r,c))
+                    row = r + dr
+                    col = c + dc
+                    if (row in range(rows) and
+                        col in range(cols) and
+                        grid[row][col] == '1' and 
+                        (row, col) not in visited):
+                        visited.add((row,col))
+                        q.append((row, col))
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    bfs(r, c)
-                    islands += 1
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    bfs(r,c)
+                    islands +=1
+
         return islands
-        
