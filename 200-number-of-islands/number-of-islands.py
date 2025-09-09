1754980b45d50f6c640bd61b58e98b40516421dd
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-09-09 19:36:54 -0400

    Time: 264 ms (35.55%) | Memory: 28.9 MB (7.62%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,29 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # 1,1,1,0
-        # 1,1,1,0
-        # 1,0,0,0
-        # 0,0,1,0
-        # 1,1,0,0
-
-        # set to track if the island is visited (m,n)
-        # dfs(m,n) in all directions
-
-        m, n = len(grid), len(grid[0])
-
+        rows, cols = len(grid), len(grid[0])
         visited = set()
+        numIslands = 0
 
-        number_of_islands = 0
-
-        def dfs(i,j):
-            if i < 0 or j < 0 or i == m or j == n or grid[i][j] == "0" or (i,j) in visited:
+        def dfs(r, c):
+            if (r < 0 or 
+                c<0 or 
+                r == rows or 
+                c == cols or 
+                grid[r][c] == '0' or 
+                (r,c) in visited):
                 return
-            else:
-                visited.add((i,j))
-                dfs(i+1,j)
-                dfs(i-1,j)
-                dfs(i,j+1)
-                dfs(i,j-1)
+            visited.add((r,c))
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
 
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == '1' and (r,c) not in visited:
+                    dfs(r,c)
+                    numIslands += 1
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and (i,j) not in visited:
-                    number_of_islands+=1
-                    dfs(i,j)
-
-        return number_of_islands
-
+        return numIslands
 
 
