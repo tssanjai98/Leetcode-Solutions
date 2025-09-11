71e613c9eeb0b46a751f40a1a85dc06152ebb0ea
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-09-11 16:30:10 -0400

    Time: 1 ms (89.21%) | Memory: 17.9 MB (26.30%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -2,38 +2,28 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         visited = set()
-        q = collections.deque()
-        mins = 0
         fresh = 0
+        mins = 0
+        q = collections.deque()
 
         for r in range(rows):
             for c in range(cols):
-                if (grid[r][c] == 2 and (r,c) not in visited):
+                if grid[r][c] == 1:
+                    fresh +=1
+                elif grid[r][c] == 2:
                     q.append((r,c))
-                    visited.add((r,c))
-                if (grid[r][c] == 1):
-                    fresh += 1
-        
-        if fresh == 0:
-            return 0
 
-        while q:
+        directions = [[1,0], [-1,0], [0,1], [0,-1]]
+        while q and fresh > 0:
             for i in range(len(q)):
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
+                r,c = q.popleft()
                 for dr, dc in directions:
-                    nr, nc = row+dr, col+dc
-                    if (nr<0 or
-                        nc<0 or
-                        nr == rows or
-                        nc == cols or
-                        grid[nr][nc] == 0 or
-                        grid[nr][nc] == 2 or
-                        (nr,nc) in visited):
-                        continue
-                    q.append((nr,nc))
-                    visited.add((nr,nc))
-                    fresh-=1
-            mins+=1
+                    nr, nc = r + dr, c + dc
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        fresh -= 1
+                        q.append((nr, nc))
+            mins += 1
 
-        return max(0,mins - 1)  if fresh == 0 else -1
+
+        return mins if fresh==0 else -1
