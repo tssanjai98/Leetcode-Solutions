128f1c50727d1313e35894bd473007f4b3268298
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-09-17 18:21:22 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.52%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -19,11 +19,12 @@ class Solution:
                 elif item != ".":
                     s.add(item)
 
+
         starts = [(0,0), (0,3), (0,6),
                   (3,0), (3,3), (3,6),
                   (6,0), (6,3), (6,6),]
 
-        for i,j in starts:
+        for i, j in starts:
             s = set()
             for row in range(i, i+3):
                 for col in range(j, j+3):
