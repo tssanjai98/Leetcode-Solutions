52d78c99b7a404b30ee7af695d22b13d840510e5
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-09-18 20:59:41 -0400

    Time: 23 ms (6.09%) | Memory: 17.9 MB (82.89%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,22 +1,22 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         i,j = 0, len(s) - 1
+
         while i < j:
-            while i < j and not self.alfaNum(s[i]):
-                i += 1
-            while j > i and not self.alfaNum(s[j]):
-                j -= 1
+            while i<j and not self.isAlphaNum(s[i]):
+                i+=1
+            while j>i and not self.isAlphaNum(s[j]):
+                j-=1
             if s[i].lower() != s[j].lower():
                 return False
             i += 1
             j -= 1
         return True
-
-    def alfaNum(self, c):
-        return (
-            ord('A') <= ord(c) <= ord('Z') or
-            ord('a') <= ord(c) <= ord('z') or
-            ord('0') <= ord(c) <= ord('9')
-        )
-
             
+
+    def isAlphaNum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9") 
+        )
