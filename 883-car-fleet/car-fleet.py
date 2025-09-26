3f1d30025232e50a18ce9dee272c4dc1d7a4c918
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-09-26 19:26:06 -0400

    Time: 167 ms (64.87%) | Memory: 39.4 MB (52.50%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -3,8 +3,10 @@ class Solution:
         pair = [(p,s) for p,s in zip(position, speed)]
         pair.sort(reverse=True)
         stack = []
+
         for p,s in pair:
             stack.append((target-p)/s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            if len(stack)>=2 and stack[-1]<=stack[-2]:
                 stack.pop()
+
         return len(stack)
