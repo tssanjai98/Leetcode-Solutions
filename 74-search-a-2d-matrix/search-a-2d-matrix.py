267614a3357173ea1d0eb886558ebd1a10c45d1c
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-10-02 23:31:02 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (72.43%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,7 +1,7 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
-        l,r = 0, ROWS*COLS -1
+        l,r = 0, ROWS*COLS-1
 
         while l <= r:
             m = l + (r-l)//2
