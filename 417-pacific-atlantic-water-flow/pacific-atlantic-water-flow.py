fba6a0bbe83838d9d6d5b961a883bf5150dda6e2
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-10-04 23:32:10 -0400

    Time: 31 ms (67.89%) | Memory: 19.4 MB (63.78%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,41 +1,37 @@
-from collections import deque
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        p_que = deque()
-        p_seen = set()
+        rows = len(heights)
+        cols = len(heights[0])
 
-        a_que = deque()
-        a_seen = set()
+        pacific_visited = set()
+        atlantic_visited = set()
+        
+        def dfs(visited, row, col, start_value):
+            if row < 0 or row >= rows or col < 0 or col >= cols:
+                return
+            
+            if (row, col) in visited:
+                return
+            
+            if heights[row][col] < start_value:
+                return
+            
+            visited.add((row, col))
+            
+            dfs(visited, row+1, col, heights[row][col]) 
+            dfs(visited, row-1, col, heights[row][col])
+            dfs(visited, row, col+1, heights[row][col])
+            dfs(visited, row, col-1, heights[row][col])
+            
+        for col in range(cols):
+            dfs(pacific_visited, 0, col, heights[0][col])
+            dfs(atlantic_visited, rows-1, col, heights[rows-1][col])
+        
+        for row in range(rows):
+            dfs(pacific_visited, row, 0, heights[row][0])
+            dfs(atlantic_visited, row, cols-1, heights[row][cols-1]) 
+        
+        output = [list(cell) for cell in pacific_visited & atlantic_visited]
 
-        m,n = len(heights), len(heights[0])
 
-        for j in range(n):
-            p_que.append((0,j))
-            p_seen.add((0,j))
-
-        for i in range(1,m):
-            p_que.append((i,0))
-            p_seen.add((i,0))
-
-        for i in range(m):
-            a_que.append((i,n-1))
-            a_seen.add((i, n-1))
-
-        for j in range(n-1):
-            a_que.append((m-1, j))
-            a_seen.add((m-1, j))
-
-        def get_coords(que, seen):
-            coords = set()
-            while que:
-                i,j = que.popleft()
-                for i_off, j_off in [(0,1), (1,0), (-1,0), (0,-1)]:
-                    r,c = i + i_off, j+j_off
-                    if 0<=r<m and 0<=c<n and heights[r][c]>=heights[i][j] and (r,c) not in seen:
-                        seen.add((r,c))
-                        que.append((r,c))
-
-        get_coords(p_que, p_seen)
-        get_coords(a_que, a_seen)
-
-        return list(p_seen.intersection(a_seen))
+        return output
