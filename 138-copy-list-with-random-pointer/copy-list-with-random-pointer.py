82178acf17db041f2aa334edd76509aa90bf5ccd
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-10-07 18:02:03 -0400

    Time: 36 ms (86.24%) | Memory: 18.6 MB (59.70%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        copyMap = {None: None}
+
+        cur = head
+        while cur:
+            copyMap[cur] = Node(cur.val)
+            cur = cur.next
+
+        cur = head
+        while cur:
+            copy = copyMap[cur]
+            copy.next = copyMap[cur.next]
+            copy.random = copyMap[cur.random]
+            cur = cur.next
+
+        return copyMap[head]
