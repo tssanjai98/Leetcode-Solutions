479df71139bde540081ae20a1a6237a942969754
Sanjai Trichinopoly Shanmugam <43338236+tssanjai98@users.noreply.github.com>
2025-11-22 01:30:57 -0500

    Time: 5 ms (57.34%) | Memory: 44 MB (68.30%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,22 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
+        if(s.length() != t.length()){
             return false;
-        }       
-
-        HashMap<Character, Integer> countS = new HashMap<>();
-        HashMap<Character, Integer> countT = new HashMap<>();
-
-        for(int i=0; i<s.length(); i++){
-            countS.put(s.charAt(i), countS.getOrDefault(s.charAt(i), 0)+1);
-            countT.put(t.charAt(i), countT.getOrDefault(t.charAt(i), 0)+1);
         }
 
-        return countS.equals(countT);
+        int[] charCount = new int[26];
+
+        for(int i=0; i<s.length(); i++) {
+            charCount[s.charAt(i) - 'a']++;
+            charCount[t.charAt(i) - 'a']--;
+        }
+
+        for(int num: charCount) {
+            if(num!=0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
